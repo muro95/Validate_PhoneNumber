3d29ae700484060d0875e9a425d7769fc499120c
muro95 <sonbena95@gmail.com>
2019-10-24 10:02:04 +0700

    first commit

---
+++ src/main/java/com/codegym/ApplicationInitializer.java
@@ -0,0 +1,23 @@
+package com.codegym;
+
+//import com.codegym.cms.ApplicationConfig;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext ctx) throws ServletException {
+        AnnotationConfigWebApplicationContext webCtx = new AnnotationConfigWebApplicationContext();
+        webCtx.register(ApplicationConfig.class);
+        webCtx.setServletContext(ctx);
+        ServletRegistration.Dynamic servlet = ctx.addServlet("dispatcher", new DispatcherServlet(webCtx));
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
+    }
+}
